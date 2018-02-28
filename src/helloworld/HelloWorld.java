33dafecfa3aa531d38a7dca12c0bcc626128623d
nitao5001 <nitao5001@7KVM6Y1.Student.UGDSB.ED>
2018-02-28 13:23:25 -0500

    First commit!

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author nitao5001
+ */
+public class HelloWorld {
+    int ageOfConsent; //[GLOBAL DECLARATION]
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //variable declaration [LOCAL DECLARATION]
+        int ageOfConsent, age, value2, value3;
+        age = 16;
+        
+        int avg;
+        avg = (80 + 56 + 75 + 82)/4;
+        
+        int remainder;
+        remainder = 27 % 5;
+        
+        int x;
+        x = 7;
+        x--;
+        
+        //String name1, name2, name;
+        //teamName = "Toronto Maple Leafs";
+        
+        //name1 = "Toronto ";
+        //name2 = "Maple Leafs";
+        //name = name1 + name2;
+        
+        System.out.println("Hello World!");
+        System.out.println(x);
+    }
+    
+}
