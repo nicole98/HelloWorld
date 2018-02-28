bd136b484e6ce58b587eac6e6d0a387b0cc4249a
nitao5001 <nitao5001@7KVM6Y1.Student.UGDSB.ED>
2018-02-28 13:33:16 -0500

    Actual HelloWorld

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -11,35 +11,13 @@ package helloworld;
  * @author nitao5001
  */
 public class HelloWorld {
-    int ageOfConsent; //[GLOBAL DECLARATION]
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //variable declaration [LOCAL DECLARATION]
-        int ageOfConsent, age, value2, value3;
-        age = 16;
-        
-        int avg;
-        avg = (80 + 56 + 75 + 82)/4;
-        
-        int remainder;
-        remainder = 27 % 5;
-        
-        int x;
-        x = 7;
-        x--;
-        
-        //String name1, name2, name;
-        //teamName = "Toronto Maple Leafs";
-        
-        //name1 = "Toronto ";
-        //name2 = "Maple Leafs";
-        //name = name1 + name2;
-        
+
         System.out.println("Hello World!");
-        System.out.println(x);
+
     }
     
 }
